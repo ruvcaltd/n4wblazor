943c39f8a196ef6707266f5e06f7bb7365c5225d
ruvcaltd <53894177+ruvcaltd@users.noreply.github.com>
2020-03-27 13:19:22 +0000

    missing files

---
+++ js/websocket.js
@@ -0,0 +1,88 @@
+﻿
+var socket;
+var scheme = document.location.protocol === "https:" ? "wss" : "ws";
+var port = document.location.port ? (":" + document.location.port) : "";
+
+connectionUrl.value = scheme + "://" + document.location.hostname + port + "/ws";
+
+function updateState() {
+    function disable() {
+        sendMessage.disabled = true;
+        sendButton.disabled = true;
+        closeButton.disabled = true;
+    }
+    function enable() {
+        sendMessage.disabled = false;
+        sendButton.disabled = false;
+        closeButton.disabled = false;
+    }
+
+    connectionUrl.disabled = true;
+    connectButton.disabled = true;
+
+    if (!socket) {
+        disable();
+    } else {
+        switch (socket.readyState) {
+            case WebSocket.CLOSED:
+                stateLabel.innerHTML = "Closed";
+                disable();
+                connectionUrl.disabled = false;
+                connectButton.disabled = false;
+                break;
+            case WebSocket.CLOSING:
+                stateLabel.innerHTML = "Closing...";
+                disable();
+                break;
+            case WebSocket.CONNECTING:
+                stateLabel.innerHTML = "Connecting...";
+                disable();
+                break;
+            case WebSocket.OPEN:
+                stateLabel.innerHTML = "Open";
+                enable();
+                break;
+            default:
+                stateLabel.innerHTML = "Unknown WebSocket State: " + htmlEscape(socket.readyState);
+                disable();
+                break;
+        }
+    }
+}
+
+function socketConnectionClose() {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert("socket not connected");
+    }
+    socket.close(1000, "Closing from client");
+}
+
+function socketConnectionSendMessage(msg) {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        console.log("socket not connected");
+    }    
+    socket.send(msg);
+}
+
+function socketConnectionConnect() {    
+    socket = new WebSocket(connectionUrl.value);
+    socket.onopen = function (event) {
+        updateState();      
+    };
+    socket.onclose = function (event) {
+        updateState();      
+    };
+    socket.onerror = updateState;
+    socket.onmessage = function (event) {
+       
+    };
+};
+
+function htmlEscape(str) {
+    return str.toString()
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
